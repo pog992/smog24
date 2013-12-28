c65fdebceee2b14cc09a3a2171d56dcb773f554a
Szymon Gut <pog992@gmail.com>
2013-12-28 02:00:29 +0100

    [CFE] backend connection updates

--- smog25/cfe/backend.py
+++ smog25/cfe/backend.py
@@ -9,34 +9,44 @@ class PickledStreamReader(object):
         self.callback = callback
         self.buffer = StringIO.StringIO()
     
-    def feed(self, data):
+    def _append(self, data):
         pos = self.buffer.tell()
         self.buffer.seek(0, 2)
         self.buffer.write(data)
         self.buffer.seek(pos)
-        try:
-            obj = pickle.load(self.buffer)
-        except (EOFError, ValueError):
-            self.buffer.seek(pos)
-        else:
-            self.callback(obj)
+
+    def feed(self, data):
+        self._append(data)
+        while True:
+            pos = self.buffer.tell()
+            try:
+                obj = pickle.load(self.buffer)
+            except (EOFError, ValueError):
+                self.buffer.seek(pos)
+                self.buffer = StringIO.StringIO(self.buffer.read())
+                break
+            else:
+                self.callback(obj)
 
 
 class BackendConnection(object):
     def __init__(self, stream):
         self.stream = stream
+        self.stream.set_close_callback(self.became_closed)
         self.reader = PickledStreamReader(self.object_received)
-        self.read_next_byte()
+        self.read_next_bytes()
     
-    def read_next_byte(self):
-        self.stream.read_bytes(1, self.data_received) 
+    def read_next_bytes(self):
+        self.stream.read_bytes(4096, self.read_next_bytes, self.reader.feed)
 
-    def data_received(self, data):
-        self.reader.feed(data)
-        self.read_next_byte()
-        
     def object_received(self, obj):
         print obj
+
+    def became_closed(self):
+        print 'closed.'
+
+    def write_object(self, obj):
+        self.stream.write(pickle.dumps(obj, pickle.HIGHEST_PROTOCOL))
 
 
 class BackendServer(tcpserver.TCPServer):
